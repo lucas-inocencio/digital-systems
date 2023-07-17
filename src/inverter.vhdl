00afdaac6bc0ce4e72702e3ad291a8b79fb41d4a
lucas-inocencio <li.lucasinocencio@gmail.com>
2023-07-17 19:23:06 -0300

    some changes.

--- src/inverter.vhdl
+++ src/inverter.vhdl
@@ -6,11 +6,11 @@ USE ieee.std_logic_1164.ALL;
 ENTITY inverter IS
     PORT (
         a : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-        y : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-    );
+        y : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
+
 END inverter;
 
 ARCHITECTURE behavior OF inverter IS
 BEGIN
-    y <= (not a) + 1;
+    y <= (NOT a) + 1;
 END behavior;
